4e3ba312a8b8b5ec7ba4406cf5cee0ac6537c188
MohamedSameer <mohamedsameeer96@gmail.com>
2019-11-25 23:41:56 +0200

    updatefile

---
+++ Java/src/Main.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        SpojSort2D2DSort ans=new SpojSort2D2DSort();
+        ans.answer();
+
+    }
+
+}
---
+++ Java/src/UVA12100PrinterQueue.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class UVA12100PrinterQueue {
+
+        void answer(){
+            Scanner input=new Scanner(System.in);
+            int t=input.nextInt();
+            ArrayL
+            while (t--!=0){
+                int n=input.nextInt();
+                int m=input.nextInt();
+                for (int i = 0; i <n ; i++) {
+
+                }
+            }
+        }
+
+}
